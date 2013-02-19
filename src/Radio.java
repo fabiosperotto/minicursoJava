ed034613cb2bb0b7c087136c2852bac534098831
fabiosperotto <fabio.aiub@gmail.com>
2013-02-19 14:23:22 -0300

    criando tudo

---
+++ src/Radio.java
@@ -0,0 +1,9 @@
+
+public interface Radio {
+
+	public void ligado();
+	public void desligado();
+	public void aumentarVolume();
+	public void diminuirVolume();
+	public void alterarFrequencia();
+}
---
+++ src/RadioAMFM.java
@@ -0,0 +1,20 @@
+
+public class RadioAMFM implements Radio {
+	
+	pblic void ligado(){
+		
+	}
+	
+	public desligado(){
+		
+	}
+	
+	public void aumentarVolume(){
+		
+	}
+	
+	public void diminuirVolume(){
+		
+	}
+
+}
