26de03511dbf34eb3d9945a4dcb5d7af24e14c89
fabiosperotto <fabio.aiub@gmail.com>
2013-02-21 16:30:08 -0300

    final aula 3

--- src/commom/graphics/TesteSwing.java
+++ src/commom/graphics/TesteSwing.java
@@ -11,18 +11,22 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import commom.radio.RadioAMFM;
+
 public class TesteSwing extends JFrame implements ActionListener{
 	
-	private JButton botao1, botao2;
+	private JButton botao1, botao2, botao3;
 	private JLabel etiqueta1, etiqueta2;
 	private JTextField campoTexto1, campoTexto2;
 	private JPanel janela;
 	private JMenuBar barraFerramentas;
 	private JMenu menu1, menu2;
 	private JMenuItem menuItem1, menuItem2;
+	private RadioAMFM radio;
 	
 	public TesteSwing(){
 		super("Testando Swing");
@@ -36,12 +40,18 @@ public class TesteSwing extends JFrame implements ActionListener{
 		campoTexto1 = new JTextField("Escreva aqui");
 		campoTexto1.setBounds(new Rectangle(70,20,90,25));
 		
-		botao1 = new JButton("Click me");
+		botao1 = new JButton("ON/OFF");
 		botao1.setBounds(new Rectangle(160,20,90,25));
 		botao1.addActionListener(this);
 		
-		botao2 = new JButton("Hi");
+		botao2 = new JButton("+");
 		botao2.setBounds(new Rectangle(80,80,50,25)); //X,Y,L,A
+		botao2.setMnemonic(KeyEvent.VK_UP);
+		botao2.addActionListener(this);
+		
+		botao3 = new JButton("-");
+		botao3.setBounds(new Rectangle(180,80,50,25));
+		botao3.addActionListener(this);
 		
 		//CRIACAO MENUBAR
 		barraFerramentas = new JMenuBar();
@@ -59,6 +69,7 @@ public class TesteSwing extends JFrame implements ActionListener{
 		
 		menuItem2 = new JMenuItem("Sobre");
 		menuItem2.setMnemonic(KeyEvent.VK_S);
+		menuItem2.addActionListener(this);
 		menu2.add(menuItem2);
 		//FIM CRIACAO MENUBAR
 		
@@ -69,6 +80,7 @@ public class TesteSwing extends JFrame implements ActionListener{
 		janela.add(campoTexto1);
 		janela.add(botao1);
 		janela.add(botao2);
+		janela.add(botao3);
 		
 		this.setJMenuBar(barraFerramentas);
 		this.getContentPane().add(janela);
@@ -78,6 +90,7 @@ public class TesteSwing extends JFrame implements ActionListener{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 		
+		
 	}
 	
 	public void actionPerformed(ActionEvent evento){
@@ -85,8 +98,31 @@ public class TesteSwing extends JFrame implements ActionListener{
 		Object acao = evento.getSource();
 		
 		if(acao == botao1){
-			campoTexto1.setText("clicado");
+			this.radio = new RadioAMFM();
+			this.radio.ligado(1);
+			campoTexto1.setText("Radio Freq. "+this.radio.getFrequencia());
 			
+		}
+		
+		if(acao == menuItem2){
+			JOptionPane.showMessageDialog(null, "URRA, aprendendo Java com Swing!");
+		}
+		
+		if(acao == botao2){
+			
+			if(this.radio != null){
+				this.radio.aumentarVolume();
+				campoTexto1.setText("Vol. "+this.radio.getVolumeAtual());
+			}
+			
+		}
+		
+		if(acao == botao3){
+			
+			if(this.radio != null){
+				this.radio.diminuirVolume();
+				campoTexto1.setText("Vol. "+this.radio.getVolumeAtual());
+			}
 		}
 		
 	}
